09c5f6f0a5ccb086fbffde4c155670c74b1acaab
stefantanasa <liketanasa@gmail.com>
2022-04-13 13:31:28 +0100

    Backoffice page

--- backend-javascript.js
+++ backend-javascript.js
@@ -1,38 +1,41 @@
 let nameField = document.querySelector("#nameField");
 let imageUrl = document.querySelector("#image-url");
 let price = document.querySelector("#price");
+let brand = document.querySelector("#brand");
 let description = document.querySelector("#product-description");
 let submitProduct = document.querySelector("#submit-form");
 let endpoint = "https://striveschool-api.herokuapp.com/api/product/";
 
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
   event.preventDefault();
   let objToSend = {
     name: nameField.value,
     price: price.value,
+    brand: brand.value,
     description: description.value,
-    imageUrl: "http://placeimg.com/640/480/sports",
+    imageUrl: imageUrl.value || "http://placeimg.com/640/480/sports",
   };
   console.log("event: ", submitProduct);
   console.log(objToSend, endpoint);
-  fetchData(objToSend);
-};
 
-submitProduct.addEventListener("submit", handleSubmit);
-
-let fetchData = async (obj) => {
   try {
     let data = await fetch(endpoint, {
       method: "POST",
-      body: JSON.stringify(obj),
+      body: JSON.stringify(objToSend),
       headers: {
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwOWZmNTRjZmY1ZjAwMTU5MGJkYWUiLCJpYXQiOjE2NDk4MzY4ODIsImV4cCI6MTY1MTA0NjQ4Mn0.972Dka7292kHJFQonsL8pN9vyYsHdhKImv5l5MKze2g",
+        "Content-Type": "Application/Json",
       },
     });
 
-    data = await data.json();
+    if (data.ok) {
+      data = await data.json();
+      alert("Product added!", data);
+    }
   } catch (error) {
     console.log(error.message);
   }
 };
+
+submitProduct.addEventListener("submit", handleSubmit);
