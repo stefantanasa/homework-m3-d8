2a9d2e7dacb6439546962519d47d301d872327ed
stefantanasa <liketanasa@gmail.com>
2022-04-15 22:47:51 +0100

    image upload fixed

--- backend-javascript.js
+++ backend-javascript.js
@@ -8,45 +8,43 @@ let backofficeArea = document.querySelector(".backoffice-area");
 let endpointPost = "https://striveschool-api.herokuapp.com/api/product/";
 let alertElement = document.createElement("div");
 console.log(nameField.value);
-const handleSubmit = async (event) => {
-  event.preventDefault();
-  let objToSend = {
-    name: nameField.value,
-    price: price.value,
-    brand: brand.value,
-    description: description.value,
-    imageUrl:
-      imageUrl.value === true
-        ? imageUrl.value
-        : "http://placeimg.com/640/480/dance",
-  };
-  console.log("event: ", submitProduct);
-  console.log(objToSend, endpointPost);
+const handleSubmit = async(event) => {
+    event.preventDefault();
+    let objToSend = {
+        name: nameField.value,
+        price: price.value,
+        brand: brand.value,
+        description: description.value,
+        imageUrl: imageUrl.value ?
+            imageUrl.value :
+            "http://placeimg.com/640/480/dance",
+    };
+    console.log("event: ", submitProduct);
+    console.log(objToSend, endpointPost);
 
-  try {
-    let data = await fetch(endpointPost, {
-      method: "POST",
-      body: JSON.stringify(objToSend),
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwOWZmNTRjZmY1ZjAwMTU5MGJkYWUiLCJpYXQiOjE2NDk4MzY4ODIsImV4cCI6MTY1MTA0NjQ4Mn0.972Dka7292kHJFQonsL8pN9vyYsHdhKImv5l5MKze2g",
-        "Content-Type": "Application/Json",
-      },
-    });
+    try {
+        let data = await fetch(endpointPost, {
+            method: "POST",
+            body: JSON.stringify(objToSend),
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwOWZmNTRjZmY1ZjAwMTU5MGJkYWUiLCJpYXQiOjE2NDk4MzY4ODIsImV4cCI6MTY1MTA0NjQ4Mn0.972Dka7292kHJFQonsL8pN9vyYsHdhKImv5l5MKze2g",
+                "Content-Type": "Application/Json",
+            },
+        });
 
-    if (data.ok) {
-      data = await data.json();
-      backofficeArea.innerHTML += `
+        if (data.ok) {
+            data = await data.json();
+            backofficeArea.innerHTML += `
       
       <div class="row alert alert-success" role="alert">
         Product Has Been Added!
       </div>
         
       `;
+        }
+    } catch (error) {
+        alert(error.message);
     }
-  } catch (error) {
-    alert(error.message);
-  }
 };
 
 submitProduct.addEventListener("submit", handleSubmit);
--- homepage.js
+++ homepage.js
@@ -5,41 +5,43 @@ let cartList = document.querySelector(".list-group-flush");
 console.log("card area: ", cardsArea);
 let items = 0;
 const handleQuickBuy = (itemId) => {
-  window.location.href = `/homework-m3-d8/sent.html?${itemId}`;
+    window.location.href = `/homework-m3-d8/sent.html?${itemId}`;
 };
 
-const handleAddCart = (event) => {
-  items++;
-  badge.textContent = `Cart ${items}`;
+const handleAddCart = (productName) => {
+    items++;
 
-  // // event.target.parentElement.parentElement.children[1].textContent.slice(26);
-
-  // productName =
-  //   event.target.parentElement.parentElement.children[0].children[1].textContent.slice(
-  //     25
-  //   );
-  // productPrice =
-  //   event.target.parentElement.parentElement.children[0].children[4].textContent.slice(
-  //     21
-  //   );
-  // items.push({ name: productName, price: productPrice });
-  // console.log(items);
-  // console.log(productName);
-
-  // let list = [];
-  // list.push(itemClicked);
-  // list.forEach((item) => {
-  //   console.log(),
-  //     (cartList.innerHTML += `<li class="added-in-the-cart">${item}</li>`);
-  // });
-  // console.log(list);
+    console.log(productName);
 };
+// badge.textContent = `Cart ${items}`;
+
+// // event.target.parentElement.parentElement.children[1].textContent.slice(26);
+
+// productName =
+//   event.target.parentElement.parentElement.children[0].children[1].textContent.slice(
+//     25
+//   );
+// productPrice =
+//   event.target.parentElement.parentElement.children[0].children[4].textContent.slice(
+//     21
+//   );
+// items.push({ name: productName, price: productPrice });
+// console.log(items);
+// console.log(productName);
+
+// let list = [];
+// list.push(itemClicked);
+// list.forEach((item) => {
+//   console.log(),
+//     (cartList.innerHTML += `<li class="added-in-the-cart">${item}</li>`);
+// });
+// console.log(list);
 
 const createCard = (arrayProducts) => {
-  arrayProducts.forEach((product) => {
-    let card = document.createElement("div");
-    card.innerHTML = `  <div class="productCard ">
-        
+    arrayProducts.forEach((product) => {
+        let card = document.createElement("div");
+        card.innerHTML = `<div class="productCard ">
+
         <a href="/homework-m3-d8/product.html?productId=${product._id}">
           <img class="img-homepage" src=${product.imageUrl} alt="" />
           <div>
@@ -56,34 +58,40 @@ const createCard = (arrayProducts) => {
           </div>
           </a>
           <div class="d-flex justify-content-center">
-            <button id="add-cart to-basket" onclick="handleAddCart(event)">Add to cart</button>
-            <a href="/homework-m3-d8/sent.html?sent=${product._id}">
+            <button id="add-cart to-basket" onclick="handleAddCart('${product.name}')">Add to cart</button>
+            <a href='/homework-m3-d8/sent.html?sent=${product._id}'>
             <button id="add-cart" type="submit">Quick Buy</button>
           </a>
-            
-          </div>
-        
-        </div>`;
 
-    cardsArea.appendChild(card);
-  });
+          </div>
+
+        </div>`;
+        //   console.log(`<div class="d-flex justify-content-center">
+        //   <button id="add-cart to-basket" onclick='handleAddCart(${product.name})'>Add to cart</button>
+        //   <a href="/homework-m3-d8/sent.html?sent=${product._id}">
+        //   <button id="add-cart" type="submit">Quick Buy</button>
+        // </a>
+
+        // </div>`);
+
+        cardsArea.appendChild(card);
+    });
 };
 
 let endpoint = "https://striveschool-api.herokuapp.com/api/product/";
 
-let fetchData = async () => {
-  let data = await fetch(endpoint, {
-    headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwOWZmNTRjZmY1ZjAwMTU5MGJkYWUiLCJpYXQiOjE2NDk4MzY4ODIsImV4cCI6MTY1MTA0NjQ4Mn0.972Dka7292kHJFQonsL8pN9vyYsHdhKImv5l5MKze2g",
-    },
-  });
+let fetchData = async() => {
+    let data = await fetch(endpoint, {
+        headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwOWZmNTRjZmY1ZjAwMTU5MGJkYWUiLCJpYXQiOjE2NDk4MzY4ODIsImV4cCI6MTY1MTA0NjQ4Mn0.972Dka7292kHJFQonsL8pN9vyYsHdhKImv5l5MKze2g",
+        },
+    });
 
-  data = await data.json();
-  console.log(data);
+    data = await data.json();
+    console.log(data);
 
-  createCard(data.reverse().slice(0, 9));
+    createCard(data.reverse().slice(0, 9));
 };
 window.onload = () => {
-  fetchData();
+    fetchData();
 };
